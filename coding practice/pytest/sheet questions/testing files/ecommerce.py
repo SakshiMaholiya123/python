73ecf5fb3495f6e33f3cf13ad5fafa7f859c5d64
Sakshi Maholiya <apurvamaholiya@gmail.com>
2026-05-12 09:29:40 +0530

    pytest modification

--- coding practice/pytest/sheet questions/testing files/ecommerce.py
+++ coding practice/pytest/sheet questions/testing files/ecommerce.py
@@ -3,10 +3,10 @@ def calculate_total(items,tax_rate):
         
     for item_price in items:
         if item_price<0:
-            raise ValueError
+            raise ValueError("Item price cannot be negative")
             
         if tax_rate<0 or tax_rate>1:
-            raise ValueError
+            raise ValueError("Invalid tax rate")
         
 
     total=sum(items)
--- coding practice/pytest/sheet questions/testing files/test_heathcare.py
+++ coding practice/pytest/sheet questions/testing files/test_heathcare.py
@@ -7,9 +7,9 @@ def test_validate_patient_success():
     assert validate_patient(120, 220) is True  
 
 def test_validate_patient_invalid_age():
-    with pytest.raises(PatientValidationError, match="invalid age: 150"):
+    with pytest.raises(PatientValidationError, match="invalid age {age} must be between 0 and 120."):
         validate_patient(150, 80)
 
 def test_validate_patient_invalid_heart_rate():
-    with pytest.raises(PatientValidationError, match="invalid heart rate: 10"):
+    with pytest.raises(PatientValidationError, match="invalid heart rate {heart_rate} must be between 20 and 220."):
         validate_patient(45, 10)
